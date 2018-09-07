20380318655f1d0d898ecf6af96e860e1f69f652
Ian Deans <ideans715@gmail.com>
2018-09-06 23:07:08 -0700

    wip add redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import createHistory from 'history/createBrowserHistory';
 import ReactDOM from 'react-dom';
+import { Route, Switch, Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 // import './index.css';
-import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import store from './store';
+import App from './containers/App';
+
+ReactDOM.render(
+    <Router history={ createHistory() }>
+        <Provider store={ store }>
+            <Switch>
+                <Route path="/" component={ App } />
+            </Switch>
+        </Provider>
+    </Router>,
+    document.getElementById( 'root' )
+);
+// registerServiceWorker();
